32ae6edd39cd9322e7007e836d188e4f21dc4d32
AlexGA93 <alexgimat@hotmail.com>
2021-09-15 09:27:53 +0200

    Setting new contact dispatching actions

--- src/reducers/ContactReducer.js
+++ src/reducers/ContactReducer.js
@@ -1,21 +1,27 @@
 // initial reducer state
 const initialState = [
     {
-        id:0,
-        name:'Alejandro Mathews',
-        email:'alexmail@mail.com',
-        age:28
+        id: 0,
+        name: 'Alejandro Mathews',
+        email: 'alexmail@mail.com',
+        age: 28
     },
     {
-        id:1,
-        name:'Tiana Doe',
-        email:'TDoe@mail.com',
-        age:31
+        id: 1,
+        name: 'Tiana Doe',
+        email: 'TDoe@mail.com',
+        age: 31
     }
 ];
 
 const contactReducer = (state = initialState, action) => {
-    switch(action.type){
+    switch (action.type) {
+        // In case our action is add type, we must update our state with the action payload and return this updated state
+
+        case "ADD_CONTACT":
+            state = [...state, action.payload];
+            return state;
+
         default:
             return state;
     }
